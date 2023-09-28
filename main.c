24e9970e90424168a025f5a8ff0c9ea3f2f1094b
Na-yunyoung <yunyoung23@sookmyung.ac.kr>
2023-09-28 20:51:17 +0900

    04-5

--- main.c
+++ main.c
@@ -4,24 +4,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a;
+	unsigned int x;
+	int b;
 	
-	printf("input a year : ");
-	scanf("%i", &a);
+	printf("input a number : ");
+	scanf("%ui", &x);
 	
-	//윤년의 조건. 
-	//1. 4로 나누어 떨어져야함
-	//2. 100으로 나누어 떨어지지 않아야함
-	//3. 위를 만족 안해도 400으로 나누어 떨어지는 경우. 
-	
-	if  ((a%4 == 0 && a%100 !=0) || (a%400==0) )
+	for (b = 0; x!=0; x>>= 1)
 	{
-		printf("%i year is leap year. \n", a);
-	 } 
-	else
-	{
-		printf("%i year is not leap year. \n", a);
+		if (x & 1)
+		{
+			b++;
+		}
 	}
-	
+	printf("The result is : %i \n", b);
 	return 0;
 }
