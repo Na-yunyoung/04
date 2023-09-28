e5d64c4a9fa8615c9d8416321c7c20dd82625e8e
Na-yunyoung <yunyoung23@sookmyung.ac.kr>
2023-09-28 20:14:08 +0900

    04-3

--- main.c
+++ main.c
@@ -4,16 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a, b;
+	int a;
 	
-	printf("input two integers : ");
-	scanf("%i %i", &a, &b);
+	printf("input the second : ");
+	scanf("%i", &a);
 	
-	printf("%i + %i = %i \n", a, b, a+b);
-	printf("%i - %i = %i \n", a, b, a-b);
-	printf("%i * %i = %i \n", a, b, a*b);
-	printf("%i / %i = %i \n", a, b, a/b);
-	printf("%i %% %i = %i \n", a, b, a%b);
+	printf("the time is %i : %i \n", a/60, a%60);
 	
 	return 0;
 }
