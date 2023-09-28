1a8c7f0105e0f1134537ffa7de9ff478285456bb
Na-yunyoung <yunyoung23@sookmyung.ac.kr>
2023-09-28 21:10:42 +0900

    04-6

--- main.c
+++ main.c
@@ -4,19 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	unsigned int x;
-	int b;
+	int a;
 	
-	printf("input a number : ");
-	scanf("%ui", &x);
+	printf("input the second : ");
+	scanf("%i", &a);
 	
-	for (b = 0; x!=0; x>>= 1)
-	{
-		if (x & 1)
-		{
-			b++;
-		}
-	}
-	printf("The result is : %i \n", b);
+	printf("The time for %i second is %i : %i : % i",a , a/3600, (a/60)%60, a%60);
+	
 	return 0;
 }
